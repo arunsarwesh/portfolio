0cbb2c11cb5444a288264a44ce63a111e3697869
SarweshwarDeivasihamani <89565470+arunsarwesh@users.noreply.github.com>
2025-01-03 22:11:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text",{
-    strings:["Frontend Developer", "Python Developer"],
+    strings:["Django Developer", "React Developer","Full Stack Developer"],
     typeSpeed : 10,
     backSpeed : 100,
     backDelay : 1000,
